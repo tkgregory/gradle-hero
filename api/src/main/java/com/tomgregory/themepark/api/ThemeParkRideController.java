8b98f2ab11b06894f619e6a8501866d6fb4e89d6
Tom Gregory <t.k.gregory@gmail.com>
2021-03-30 08:02:09 +0100

    Add UI submodule.

--- api/src/main/java/com/tomgregory/themepark/api/ThemeParkRideController.java
+++ api/src/main/java/com/tomgregory/themepark/api/ThemeParkRideController.java
@@ -2,12 +2,14 @@ package com.tomgregory.themepark.api;
 
 import com.tomgregory.themepark.db.entity.ThemeParkRide;
 import com.tomgregory.themepark.db.repository.ThemeParkRideRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Iterator;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ThemeParkRideController {
 
     private final ThemeParkRideRepository themeParkRideRepository;
