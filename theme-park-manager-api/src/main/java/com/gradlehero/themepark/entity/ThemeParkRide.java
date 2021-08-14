5f6d85af2b58dbb84b8ae28b94254475be9f055b
Tom Gregory <t.k.gregory@gmail.com>
2021-08-14 18:51:27 +0100

    Use Records instead of Lombok.

--- theme-park-manager-api/src/main/java/com/gradlehero/themepark/entity/ThemeParkRide.java
+++ theme-park-manager-api/src/main/java/com/gradlehero/themepark/entity/ThemeParkRide.java
@@ -1,13 +1,4 @@
 package com.gradlehero.themepark.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@ToString
-@AllArgsConstructor
-@Getter
-public class ThemeParkRide {
-    private String name;
-    private String description;
+public record ThemeParkRide(String name, String description) {
 }
